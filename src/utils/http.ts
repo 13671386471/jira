400998355de760128083636b812d26a7bf415fad
liqiankun <13671386471@163.com>
2024-04-22 22:59:23 +0800

    5-8 TS的联合类型、Partial和Omit介绍

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -50,6 +50,56 @@ export const http = (
 export const useHttp= () => {
     const {user} = useAuth();
     // return ([endpoint, config]: [string, HttpConfig]) => http(endpoint, config)
-    // TODO Parameters TS操作符讲解
+    // TODO Parameters TS操作符讲解 --- utility Types
+
+    // typeof http 中的typeof是ts中的类型操作符，用来获取一个函数的参数类型
+    // typeof 1 === 'number' 是在运行时执行的；
+    // ts中的typeof是在静态时候执行的(即编写代码时)，并且执行时候是没有这些代码的
+    // utility Types 的用法：用泛型给他传入其他类型，然后utility Types可以这些类型进行对应的操作
     return (...[endpoint, config]: Parameters<typeof http>) => http(endpoint, {...config, token: user?.token})
 }
+
+type Person={
+    name: string;
+    age: number;
+}
+const per: Partial<Person> = {age: 12}
+const shenmiren: Omit<Person, 'name'> = {age: 12}
+const shenmiren2: Omit<Person, 'name'|'age'> = {123: '123'}
+
+
+// 联合类型
+let param: number | string;
+param=1;
+param='sring'
+
+// 如果多个变量类型都是 number | string，
+// 那么可以写成【类型别名】 type classAll= number | string
+// type classAll = number | string;
+// let params: classAll = 1;
+
+
+// // 类型别名和接口interface很像
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+// let xiaoming: Person = {
+//     name: 'xiaoming',
+//     age: 18
+// }
+// // 用类型别名也能实现上面的效果
+// type Person2 = {
+//     name: string;
+//     age: number;
+// }
+// let xiaohua: Person2 = {
+//     name: 'xiaohua',
+//     age: 18
+// }
+
+// 类型别名很多情况下可以和interface互换的；
+// 类型别名和接口的区别：
+// 1. 【类型别名】可以重复定义；2. 【】类型别名不能继承；
+// 3、interface不能像类型别名那样定义联合类型、交叉类型；
+// 4、interface没法实现utility Types; type可以；Partial<T> Parameters<>
