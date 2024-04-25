11faf2b3018e1b8d54a6d032818d260756127a2b
liqiankun <13671386471@163.com>
2024-04-25 08:42:44 +0800

    6-3gride布局项目列表知识点

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -7,25 +7,50 @@ import styled from '@emotion/styled';
 export const AuthenticatedApp = () => {
     const {logout} = useAuth();
     return (
-        <div>
+        <Container>
             <PageHeader>
                 <button onClick={logout}>
-                    退出
+                    退出12
                 </button>
             </PageHeader>
+            <Nav >Nav </Nav>
             <Main>
                 <ProjectScreen />
             </Main>
-           
-        </div>
+            <Aside >Aside</Aside>
+            <Footer >Footer</Footer>
+        </Container>
     )
 }
 
-const PageHeader = styled.div`
-    height: 6rem;
+const Container = styled.div`
+    display: grid;
+    grid-template-rows: 6rem 1fr 6rem;// 意思是第一行占6rem，第二行占1fr(减去第一行第三行的高度后沾满)，第三行占6rem
+    grid-template-columns: 20rem 1fr 20rem;// 意思是第一列占6rem，第二列占1fr(减去第一列第三列的宽度后沾满)，第三列占20rem
+    grid-template-areas: 
+        "header header header"
+        "nav main aside"
+        "footer footer footer";
+    height: 100vh;
+`
+const PageHeader = styled.header`
+    grid-area: header;
     background-color: gray;
 `
-const Main = styled.div`
-    height: calc(100vh - 6rem);
+const Nav = styled.nav`
+    grid-area: nav;
+    background-color: #eee;
+`
+const Main = styled.main`
+    grid-area: main;
+    height: calc(100vh - 12rem);
     /* background-color: #eee; */
+`
+const Aside = styled.aside`
+    grid-area: aside;
+    background-color: #eee;
+`
+const Footer = styled.footer`
+    grid-area: footer;
+    background-color: #eee;
 `
