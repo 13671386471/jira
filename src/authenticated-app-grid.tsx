a1406a52632e5ae7d2acdda7f5014361ea9f7150
liqiankun <13671386471@163.com>
2024-04-25 21:16:12 +0800

    6-5 用CSS-in-JS状态工程创建自定义组件

---
+++ src/authenticated-app-grid.tsx
@@ -0,0 +1,86 @@
+
+import React, {useEffect} from 'react';
+import { ProjectScreen } from 'screeen/project-list';
+import { useAuth } from 'context/auth-context';
+import styled from '@emotion/styled';
+
+
+/**
+ * grid 和 flex 各自的应用场景
+ * 1. 要考虑，是一维布局 还是 二维布局
+ * 一般来说，一维布局用flex，二维布局用grid
+ * 2. 是从内容出发还是从布局出发？
+ * 从内容出发：你先有一组内容(数量一般不固定),然后希望他们均匀的分布在容器中，由内容自己的大小决定占据的空间
+ * 从布局出发：先规划网格(数量一般比较固定)，然后再把元素往里填充
+ * 从内容出发，用flex
+ * 从布局出发，用grid
+ *
+ */
+export const AuthenticatedApp = () => {
+    const {logout} = useAuth();
+    return (
+        <Container>
+            <PageHeader>
+                <PageHeaderLeft>
+                    <span>logo</span>
+                    <h1 style={{margin: 0}}>项目列表</h1>
+
+                </PageHeaderLeft>
+                <button onClick={logout}>
+                    登出
+                </button>
+            </PageHeader>
+            <Nav >Nav </Nav>
+            <Main>
+                <ProjectScreen />
+            </Main>
+            <Aside >Aside</Aside>
+            <Footer >Footer</Footer>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    display: grid;
+    grid-template-rows: 6rem 1fr 6rem;// 意思是第一行占6rem，第二行占1fr(减去第一行第三行的高度后沾满)，第三行占6rem
+    grid-template-columns: 20rem 1fr 20rem;// 意思是第一列占6rem，第二列占1fr(减去第一列第三列的宽度后沾满)，第三列占20rem
+    grid-template-areas: 
+        "header header header"
+        "nav main aside"
+        "footer footer footer";
+    height: 100vh;
+    /* grid-gap: 10rem; */
+`
+const PageHeader = styled.header`
+    grid-area: header;
+    background-color: gray;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+const PageHeaderLeft = styled.div`
+    display: flex;
+    align-items: center;
+`
+const PageHeaderRight = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+const Nav = styled.nav`
+    grid-area: nav;
+    background-color: #eee;
+`
+const Main = styled.main`
+    grid-area: main;
+    height: calc(100vh - 12rem);
+    /* background-color: #eee; */
+`
+const Aside = styled.aside`
+    grid-area: aside;
+    background-color: #eee;
+`
+const Footer = styled.footer`
+    grid-area: footer;
+    background-color: #eee;
+`
--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,56 +1,62 @@
 
 import React, {useEffect} from 'react';
+import { Button } from 'antd';
 import { ProjectScreen } from 'screeen/project-list';
 import { useAuth } from 'context/auth-context';
 import styled from '@emotion/styled';
+import { Row } from 'components/lib';
 
+
+/**
+ * grid 和 flex 各自的应用场景
+ * 1. 要考虑，是一维布局 还是 二维布局
+ * 一般来说，一维布局用flex，二维布局用grid
+ * 2. 是从内容出发还是从布局出发？
+ * 从内容出发：你先有一组内容(数量一般不固定),然后希望他们均匀的分布在容器中，由内容自己的大小决定占据的空间
+ * 从布局出发：先规划网格(数量一般比较固定)，然后再把元素往里填充
+ * 从内容出发，用flex
+ * 从布局出发，用grid
+ *
+ */
 export const AuthenticatedApp = () => {
     const {logout} = useAuth();
     return (
         <Container>
-            <PageHeader>
-                <button onClick={logout}>
-                    退出12
-                </button>
+            <PageHeader between={true}>
+                <PageHeaderLeft gap={3}>
+                    <span>logo</span>
+                    <h1>项目列表</h1>
+                    <h1>用户</h1>
+                </PageHeaderLeft>
+                <PageHeaderRight>
+                    <Button onClick={logout}>
+                        登出
+                    </Button>
+                </PageHeaderRight>
+               
             </PageHeader>
-            <Nav >Nav </Nav>
             <Main>
                 <ProjectScreen />
             </Main>
-            <Aside >Aside</Aside>
-            <Footer >Footer</Footer>
         </Container>
     )
 }
 
 const Container = styled.div`
     display: grid;
-    grid-template-rows: 6rem 1fr 6rem;// 意思是第一行占6rem，第二行占1fr(减去第一行第三行的高度后沾满)，第三行占6rem
-    grid-template-columns: 20rem 1fr 20rem;// 意思是第一列占6rem，第二列占1fr(减去第一列第三列的宽度后沾满)，第三列占20rem
-    grid-template-areas: 
-        "header header header"
-        "nav main aside"
-        "footer footer footer";
+    grid-template-rows: 6rem 1fr;// 意思是第一行占6rem，第二行占1fr(减去第一行的高度后沾满)
     height: 100vh;
 `
-const PageHeader = styled.header`
-    grid-area: header;
-    background-color: gray;
-`
-const Nav = styled.nav`
-    grid-area: nav;
-    background-color: #eee;
+const PageHeader = styled(Row)``
+
+const PageHeaderLeft = styled(Row)``
+const PageHeaderRight = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `
 const Main = styled.main`
-    grid-area: main;
-    height: calc(100vh - 12rem);
+    /* grid-area: main; // 可以把这里打开看看效果
+    height: calc(100vh - 6rem); */
     /* background-color: #eee; */
-`
-const Aside = styled.aside`
-    grid-area: aside;
-    background-color: #eee;
-`
-const Footer = styled.footer`
-    grid-area: footer;
-    background-color: #eee;
 `
