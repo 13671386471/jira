fd63e915816e426e74d6a81f5a01f3037eef5b28
liqiankun <13671386471@163.com>
2024-04-28 23:27:35 +0800

    fetch请求后为何要res.json() 这样操作的注释

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -24,6 +24,15 @@ export const login = (data: {username: string, password: string}) => {
         console.log('login>', res);
         if (res.ok) {
             return handleUserResponse(await res.json())
+            //在使用 fetch 发起HTTP请求时，fetch 函数返回的是一个 Promise，
+            // 这个 Promise 会在请求完成时解析为一个 Response 对象。
+            // 这个 Response 对象代表了从服务器接收到的完整响应，
+            // 包括但不限于HTTP状态码、响应头和响应体等信息。
+            // 但需要注意的是，Response 对象本身并不直接包含可读的JSON数据或文本数据，
+            // 这些数据实际上是存储在响应体（body）中。
+            //当你需要从响应体中提取JSON格式的数据时，就需要使用 res.json() 方法。这个方法同样返回一个 Promise，该 Promise 解析后会得到一个JavaScript对象，这个对象是根据响应体中的JSON数据构建的。因此，你需要使用 await 关键字等待 res.json() 的完成，以便获取到实际的JSON数据。
+
+
         } else {
             return Promise.reject(await res.json())
         }
