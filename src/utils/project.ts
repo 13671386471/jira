bdc0c45805b29b9787c39d58c8802f163e16d2d2
liqiankun <13671386471@163.com>
2024-05-05 16:07:37 +0800

    10-1使用useCallBack解决依赖非状态的非基本类型变量造成的循环触发执行问题

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -10,6 +10,8 @@ export const useProject = (param: Partial<Project>) => {
     const fetchProject = () => ajax('projects', { data: clearnObject(param)})
     useEffect(() => {
         run(fetchProject(), {retry: fetchProject})
+        // 视频的react版本中这里ESlint提示要依赖run, 因为useEffect用到了就要依赖run，当run更新的话就重新执行useEffect并且用最新的run方法
+        // 但是现在最新的react版本中，useEffect中不依赖run，ESlint也不报错了
     }, [param])// param发生变化时候请求列表数据，但是param是根据录入实时变化的，但是录入项目名称时候只需要录入完成后再出发即可，所以用debounceValue做为触发条件
 
     return result
